06b3112901978d2c0121aacef0573b93d6a002b4
Jawad Ahmed <34104787+jawad-ahmed23@users.noreply.github.com>
2023-09-08 12:26:50 +0500

    add animation for news page

--- src/components/News/index.tsx
+++ src/components/News/index.tsx
@@ -10,6 +10,51 @@ import {
 import { Footer, Nav } from "..";
 import ArticalCard from "./ArticalCard";
 
+const Fade = require("react-reveal/Fade");
+
+const data = [
+  {
+    id: 1,
+    imageUrl: "/assets/news/1.jpg",
+  },
+  {
+    id: 2,
+    imageUrl: "/assets/news/2.jpg",
+  },
+  {
+    id: 3,
+    imageUrl: "/assets/news/3.jpg",
+  },
+  {
+    id: 4,
+    imageUrl: "/assets/news/4.jpg",
+  },
+  {
+    id: 5,
+    imageUrl: "/assets/news/5.jpg",
+  },
+  {
+    id: 6,
+    imageUrl: "/assets/news/6.jpg",
+  },
+  {
+    id: 7,
+    imageUrl: "/assets/news/7.jpg",
+  },
+  {
+    id: 8,
+    imageUrl: "/assets/news/8.jpg",
+  },
+  {
+    id: 9,
+    imageUrl: "/assets/news/9.jpg",
+  },
+  {
+    id: 10,
+    imageUrl: "/assets/news/10.jpg",
+  },
+];
+
 export default function News() {
   return (
     <>
@@ -35,101 +80,37 @@ export default function News() {
           zIndex={-1}
         />
         <Box textAlign="center" mt={{ base: "40px", md: "70px" }} mb="70px">
-          <Text
-            as="h1"
-            mb="3"
-            fontSize="sm"
-            textTransform="uppercase"
-            letterSpacing="2px"
-          >
-            News
-          </Text>
-          <Text fontSize="3xl" fontWeight={500}>
-            Lorem Ipsum
-          </Text>
+          <Fade bottom>
+            <Text
+              as="h1"
+              mb="3"
+              fontSize="sm"
+              textTransform="uppercase"
+              letterSpacing="2px"
+            >
+              News
+            </Text>
+          </Fade>
+          <Fade bottom>
+            <Text fontSize="3xl" fontWeight={500}>
+              Lorem Ipsum
+            </Text>
+          </Fade>
         </Box>
         <Container id="phron-features" maxW="1160px" pb="100px">
           <Wrap justify="center" spacing="44px">
-            <WrapItem>
-              <ArticalCard
-                buttonHref="#"
-                description="Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem"
-                title="Lorem ipsum upum ipopun impus"
-                imageUrl="/assets/news/1.jpg"
-              />
-            </WrapItem>
-            <WrapItem>
-              <ArticalCard
-                buttonHref="#"
-                description="Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem"
-                title="Lorem ipsum upum ipopun impus"
-                imageUrl="/assets/news/2.jpg"
-              />
-            </WrapItem>
-            <WrapItem>
-              <ArticalCard
-                buttonHref="#"
-                description="Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem"
-                title="Lorem ipsum upum ipopun impus"
-                imageUrl="/assets/news/3.jpg"
-              />
-            </WrapItem>
-            <WrapItem>
-              <ArticalCard
-                buttonHref="#"
-                description="Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem"
-                title="Lorem ipsum upum ipopun impus"
-                imageUrl="/assets/news/4.jpg"
-              />
-            </WrapItem>
-            <WrapItem>
-              <ArticalCard
-                buttonHref="#"
-                description="Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem"
-                title="Lorem ipsum upum ipopun impus"
-                imageUrl="/assets/news/5.jpg"
-              />
-            </WrapItem>
-            <WrapItem>
-              <ArticalCard
-                buttonHref="#"
-                description="Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem"
-                title="Lorem ipsum upum ipopun impus"
-                imageUrl="/assets/news/6.jpg"
-              />
-            </WrapItem>
-            <WrapItem>
-              <ArticalCard
-                buttonHref="#"
-                description="Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem"
-                title="Lorem ipsum upum ipopun impus"
-                imageUrl="/assets/news/7.jpg"
-              />
-            </WrapItem>
-            <WrapItem>
-              <ArticalCard
-                buttonHref="#"
-                description="Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem"
-                title="Lorem ipsum upum ipopun impus"
-                imageUrl="/assets/news/8.jpg"
-              />
-            </WrapItem>
-            <WrapItem>
-              <ArticalCard
-                buttonHref="#"
-                description="Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem"
-                title="Lorem ipsum upum ipopun impus"
-                imageUrl="/assets/news/9.jpg"
-              />
-            </WrapItem>
-            <WrapItem>
-              <ArticalCard
-                buttonHref="#"
-                description="Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem"
-                title="Lorem ipsum upum ipopun impus"
-                imageUrl="/assets/news/10.jpg"
-              />
-            </WrapItem>
+            {data.map((d) => (
+              <WrapItem key={d.id}>
+                <Fade bottom>
+                  <ArticalCard
+                    buttonHref="#"
+                    description="Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem"
+                    title="Lorem ipsum upum ipopun impus"
+                    imageUrl={d.imageUrl}
+                  />
+                </Fade>
+              </WrapItem>
+            ))}
           </Wrap>
         </Container>
       </Box>
