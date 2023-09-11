dbe2037432516f16c1f13daa229c6168171f6c9b
Jawad Ahmed <34104787+jawad-ahmed23@users.noreply.github.com>
2023-09-11 17:31:06 +0500

    add animation

--- src/components/PhronesesChain/PhronesisEquals/index.tsx
+++ src/components/PhronesesChain/PhronesisEquals/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Image from "next/image";
 import { Box, Text, Container, Grid, GridItem, VStack } from "@chakra-ui/react";
 
+const Fade = require("react-reveal/Fade");
+
 export default function PhronesisEquals() {
   return (
     <Box
@@ -20,16 +22,18 @@ export default function PhronesisEquals() {
       }}
     >
       <Container maxW="1160px" zIndex={1} position="relative">
-        <Text
-          as="h2"
-          fontSize={{ base: "5xl", md: "6xl" }}
-          lineHeight={{ base: "50px", md: "60px" }}
-          fontWeight={500}
-          textAlign="center"
-          mb={{ base: "20", lg: "10" }}
-        >
-          Phronesis=
-        </Text>
+        <Fade bottom>
+          <Text
+            as="h2"
+            fontSize={{ base: "5xl", md: "6xl" }}
+            lineHeight={{ base: "50px", md: "60px" }}
+            fontWeight={500}
+            textAlign="center"
+            mb={{ base: "20", lg: "10" }}
+          >
+            Phronesis=
+          </Text>
+        </Fade>
         <Grid
           templateColumns={{
             base: "repeat(1, 1fr)",
@@ -40,54 +44,60 @@ export default function PhronesisEquals() {
           rowGap="50px"
         >
           <GridItem>
-            <VStack spacing="10">
-              <Box maxH="210px" overflow="hidden">
-                <Image
-                  src="/assets/chain/shophia.png"
-                  width={214}
-                  height={230}
-                  alt="sophia image"
-                />
-              </Box>
-              <Text
-                fontWeight={500}
-                textAlign="center"
-                fontSize="lg"
-                maxW="260px"
-              >
-                Sophia: Artificial Intelligence StaAsAcal Consensus Mechanism
-              </Text>
-            </VStack>
+            <Fade bottom>
+              <VStack spacing="10">
+                <Box maxH="210px" overflow="hidden">
+                  <Image
+                    src="/assets/chain/shophia.png"
+                    width={214}
+                    height={230}
+                    alt="sophia image"
+                  />
+                </Box>
+                <Text
+                  fontWeight={500}
+                  textAlign="center"
+                  fontSize="lg"
+                  maxW="260px"
+                >
+                  Sophia: Artificial Intelligence StaAsAcal Consensus Mechanism
+                </Text>
+              </VStack>
+            </Fade>
           </GridItem>
           <GridItem>
-            <VStack spacing="10">
-              <Box pt="5">
-                <Image
-                  src="/assets/chain/protocol.png"
-                  width={208}
-                  height={228}
-                  alt="protol image"
-                />
-              </Box>
-              <Text fontWeight={500} textAlign="center" fontSize="lg">
-                Indirect <br /> - LTFM Protocol
-              </Text>
-            </VStack>
+            <Fade bottom delay={200}>
+              <VStack spacing="10">
+                <Box pt="5">
+                  <Image
+                    src="/assets/chain/protocol.png"
+                    width={208}
+                    height={228}
+                    alt="protol image"
+                  />
+                </Box>
+                <Text fontWeight={500} textAlign="center" fontSize="lg">
+                  Indirect <br /> - LTFM Protocol
+                </Text>
+              </VStack>
+            </Fade>
           </GridItem>
           <GridItem>
-            <VStack spacing="10">
-              <Box maxH="210px" overflow="hidden">
-                <Image
-                  src="/assets/chain/hand.png"
-                  width={200}
-                  height={226}
-                  alt="a hand with dollar icon in it"
-                />
-              </Box>
-              <Text fontWeight={500} textAlign="center" fontSize="lg">
-                Lucky Transactions <br /> Protocol
-              </Text>
-            </VStack>
+            <Fade bottom delay={300}>
+              <VStack spacing="10">
+                <Box maxH="210px" overflow="hidden">
+                  <Image
+                    src="/assets/chain/hand.png"
+                    width={200}
+                    height={226}
+                    alt="a hand with dollar icon in it"
+                  />
+                </Box>
+                <Text fontWeight={500} textAlign="center" fontSize="lg">
+                  Lucky Transactions <br /> Protocol
+                </Text>
+              </VStack>
+            </Fade>
           </GridItem>
         </Grid>
       </Container>
