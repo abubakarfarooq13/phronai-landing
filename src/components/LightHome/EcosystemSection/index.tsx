7a9d22c6a140f671293f267a31182e158c006878
Jawad Ahmed <34104787+jawad-ahmed23@users.noreply.github.com>
2024-10-09 13:17:00 +0500

    add hacken audit banner

--- src/components/LightHome/EcosystemSection/index.tsx
+++ src/components/LightHome/EcosystemSection/index.tsx
@@ -1,7 +1,17 @@
 import React from "react";
-import { Box, Container, Grid, GridItem, HStack, Text } from "@chakra-ui/react";
-import { IoArrowForwardCircleOutline } from "react-icons/io5";
 import Link from "next/link";
+import Image from "next/image";
+import {
+  Box,
+  Container,
+  Divider,
+  Grid,
+  GridItem,
+  HStack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { IoArrowForwardCircleOutline } from "react-icons/io5";
 
 const Fade = require("react-reveal/Fade");
 
@@ -162,6 +172,81 @@ export default function ExploreEcosystem() {
               <IoArrowForwardCircleOutline fontSize="18px" />
             </HStack>
           </Box>
+        </GridItem>
+
+        <GridItem
+          colSpan={{ base: 1, md: 3 }}
+          // bgImage="linear-gradient(145deg, rgba(176,255,231,1) 0%, rgba(121,247,209,1) 40%)"
+          bgImage="/assets/hacken-bg.png"
+          bgPos="center"
+          bgSize="cover"
+          rounded="20px"
+          position="relative"
+          // minH="300px"
+          p="30px"
+        >
+          <Box
+            position="absolute"
+            maxW={{ base: "150px", sm: "200px", md: "280px", lg: "330px" }}
+            right="0px"
+            top="0px"
+          >
+            <Image
+              src="/assets/hacken-cube.png"
+              alt="hacken-cube"
+              width={350}
+              height={350}
+            />
+          </Box>
+          <VStack
+            spacing="4"
+            align="flex-start"
+            zIndex={99}
+            position="relative"
+          >
+            <Image
+              src="/assets/new-home/Hacken.png"
+              alt="Hacken logo"
+              width={90}
+              height={55}
+              // className="logo-image"
+              style={{
+                maxWidth: "50px",
+                filter: "brightness(0%)",
+              }}
+            />
+
+            <Divider
+              borderColor="#000"
+              borderWidth={{ base: "1px", md: "1.5px" }}
+              maxW={{ base: "100%", sm: "60%" }}
+              opacity="1"
+            />
+
+            <Text
+              fontSize={{ base: "22px", md: "32px" }}
+              fontWeight={400}
+              lineHeight="120%"
+            >
+              Protocol and Tokenomics Audit{" "}
+              <Box as="br" display={{ base: "none", md: "block" }} /> of
+              PhronAI&apos;s AI-Powered Layer 0 / Layer 1
+              <Box as="br" display={{ base: "none", md: "block" }} /> Blockchain
+            </Text>
+          </VStack>
+          <HStack
+            className="arrow-btn"
+            position="absolute"
+            bottom="25px"
+            right={{ base: "30px" }}
+            as={Link}
+            href="/ecosystem"
+          >
+            <Text fontSize="14px" fontWeight={500}>
+              Explore Ecosystem
+            </Text>
+            <IoArrowForwardCircleOutline fontSize="18px" />
+          </HStack>
         </GridItem>
       </Grid>
     </Container>
