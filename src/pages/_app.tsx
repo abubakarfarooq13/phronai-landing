95c4803e401f00387cd27e1de4e1099eff5e03f6
abubakarfarooq13 <abubakarfarooq13@gmail.com>
2025-04-10 17:30:58 +0500

    sco fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -66,7 +66,7 @@ export default function App({ Component, pageProps, cookies }: MyAppProps) {
         <meta name="robots" content="index, follow" />
 
         {/* Canonical URL */}
-        <link rel="canonical" href="https://phronai.vercel.app" />
+        <link rel="canonical" href="https://phron.ai/" />
 
         {/* Favicons */}
         {/* <link rel="icon" href="/favicon.ico" /> */}
@@ -74,18 +74,18 @@ export default function App({ Component, pageProps, cookies }: MyAppProps) {
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
+          href="https://phron.ai/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png"
+          href="https://phron.ai/favicon-16x16.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href="https://phron.ai/apple-touch-icon.png"
         />
         {/* <link rel="manifest" href="/site.webmanifest" /> */}
         <meta name="theme-color" content="#000000" />
@@ -94,7 +94,7 @@ export default function App({ Component, pageProps, cookies }: MyAppProps) {
         <meta property="og:locale" content="en_US" />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="PhronAI" />
-        <meta property="og:url" content="https://phronai.vercel.app" />
+        <meta property="og:url" content="https://phron.ai/" />
         <meta
           property="og:title"
           content="AI-Powered for Builders. One Prompt for Everyone."
@@ -103,10 +103,7 @@ export default function App({ Component, pageProps, cookies }: MyAppProps) {
           property="og:description"
           content="Train AI models and earn from the network. Launch L1s, deploy smart contracts, leverage AI Agents to manage your contracts, and create AI-powered Oracles — all with just a prompt."
         />
-        <meta
-          property="og:image"
-          content="https://phronai.vercel.app/assets/cover.png"
-        />
+        <meta property="og:image" content="https://phron.ai/assets/cover.png" />
         <meta property="og:image:type" content="image/png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
@@ -115,7 +112,7 @@ export default function App({ Component, pageProps, cookies }: MyAppProps) {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@Phron_ai" />
         <meta name="twitter:creator" content="@Phron_ai" />
-        <meta name="twitter:url" content="https://phronai.vercel.app" />
+        <meta name="twitter:url" content="https://phron.ai/" />
         <meta
           name="twitter:title"
           content="AI-Powered for Builders. One Prompt for Everyone."
@@ -126,7 +123,7 @@ export default function App({ Component, pageProps, cookies }: MyAppProps) {
         />
         <meta
           name="twitter:image"
-          content="https://phronai.vercel.app/assets/cover.png"
+          content="https://phron.ai/assets/cover.png"
         />
       </Head>
       <div className={`${ruberoidFont.variable}`}>
